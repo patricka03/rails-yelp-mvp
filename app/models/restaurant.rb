1e7581344ba07814d4515ea81e9165b9043c9806
Patrick Amfo <patrick.amfo@icloud.com>
2025-04-26 01:20:33 +0100

    Added seed file

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,5 +1,5 @@
 class Restaurant < ApplicationRecord
-  has_many :review, dependent: :destroy
-  validates :name, :address, :category, :phone_number, presence: true
+  has_many :reviews, dependent: :destroy
+  validates :name, :address, :category, presence: true
   validates :category, inclusion: { in: ["chinese", "italian", "japanese", "french", "belgian"] }
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,36 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+p 'Deleting the database'
+Restaurant.destroy_all
+
+p 'Adding new restaurants to the database'
+
+house_of_ming = Restaurant.new(name: "House of Ming", address: "China", category: "chinese")
+house_of_ming.save
+yummy = Review.new(content: "brilliant food and service, highlighting the dumplings and noodles as exceptional", rating: 5)
+yummy.restaurant = house_of_ming
+yummy.save
+
+tozi_restaurant_bar = Restaurant.new(name: "TOZI Restaurant & Bar", address: "Italy", category: "italian")
+tozi_restaurant_bar.save
+yummy1 = Review.new(content: "friendly staff, nice and relaxing ambience, great food, and appreciated the quick service", rating: 4)
+yummy1.restaurant = tozi_restaurant_bar
+yummy1.save
+
+zuma_london = Restaurant.new(name: "Zuma London", address: "London", category: "japanese")
+zuma_london.save
+yummy2 = Review.new(content: "excellent quality sushi and recommended the tasting menu as good value for money", rating: 4)
+yummy2.restaurant = zuma_london
+yummy2.save
+
+clos_maggiore = Restaurant.new(name: "Clos Maggiore", address: "France", category: "french")
+clos_maggiore.save
+yummy3 = Review.new(content: "beautiful ambiance, delicious cuisine, and wonderful service, perfect for a romantic setting", rating: 3)
+yummy3.restaurant = clos_maggiore
+yummy3.save
+
+lowlander_grand_cafe = Restaurant.new(name: "Lowlander Grand Cafe", address: "Belgium Street", category: "belgian")
+lowlander_grand_cafe.save
+yummy4 = Review.new(content: "I enjoyed the great selection of beers and the atmosphere as lively and inviting", rating: 5)
+yummy4.restaurant = lowlander_grand_cafe
+yummy4.save
+
+p 'Restaurants have been all addedd'
